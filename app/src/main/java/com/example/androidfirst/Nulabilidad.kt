a13de1dc3125e965d8e0f5692b2969b317c38e2f
martin olmos <martino@epidataconsulting.com>
2024-05-13 19:33:40 -0300

    ejemplo de nulabilidad

---
+++ app/src/main/java/com/example/androidfirst/Nulabilidad.kt
@@ -0,0 +1,17 @@
+package com.example.androidfirst
+
+fun main () {
+    var variable:String? = null;
+
+    println(variable?.get(0)); // si es null me dice null pero no se rompe, si no es null me trae el primer caracter
+
+    println(variable?.get(0) ?: "es null"); // con el operador elvis puedo poner si es null (solo chequea null) que haga otra cosa
+
+    println(null?:"algo")
+
+    println(variable!!.get(1)); //cuando le pongo  !! estoy diciendo q no es null , si es null va a romper
+
+
+
+}
+
