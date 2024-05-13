4e19543c1c74965b909157e4c0a0a592a8afc5c0
martin olmos <martino@epidataconsulting.com>
2024-05-13 19:19:25 -0300

    ejemplo uso de when

---
+++ app/src/main/java/com/example/androidfirst/whenExamples.kt
@@ -0,0 +1,52 @@
+package com.example.androidfirst
+
+fun main() {
+    println(findMonth(12));
+    println(encontrarTrimestre(8));
+    println(encontrarSemestre(7));
+    resultado("5");
+}
+
+fun findMonth (month:Int):String {
+    return when (month){
+        1 -> {
+            return "Enero" // si lo pongo entre llave puedo hacer varias cosas en el caso 1
+        }
+        2 -> "Febrero"
+        3 -> "Marzo"
+        4-> "Abril"
+        5 -> "Mayo"
+        6 -> "Junio"
+        7 -> "Julio"
+        8 -> "Agosto"
+        9 -> "Septiembre"
+        10 -> "Octubre"
+        11 -> "Noviembre"
+        12 -> "Diciembre"
+        else -> "error"
+    }
+}
+
+fun encontrarTrimestre (month:Int):String = when (month){
+        1, 2, 3 -> "primer trimestre"
+        4, 5, 6 -> "segundo trimestre"
+        7, 8, 9 -> "tercero trimestre"
+        10, 11, 12 -> "cuarto trimestre"
+        else -> "error"
+}
+
+fun encontrarSemestre (month:Int):String = when (month){
+        in 1..6 -> "primer semestre"
+        in 7..12 -> "segundo semestre"
+        !in 1..12 -> "no es un numer válido"
+        else -> "error"
+}
+
+fun resultado (value:Any):Unit { //Unit es como void
+    when (value) {
+        is Int -> println(value + value) //puedo chequear qué tipo de datos es con in Tipo
+        is String -> println(value)
+        is Boolean -> if (value) println("es verdadero")
+        else -> println("otro tipo")
+    }
+}
