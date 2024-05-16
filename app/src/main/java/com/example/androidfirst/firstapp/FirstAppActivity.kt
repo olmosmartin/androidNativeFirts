5d43c5fc4281d12c6f211fc82084ff18ea3b5b2d
martin olmos <martino@epidataconsulting.com>
2024-05-16 20:54:55 -0300

    hola mundo

---
+++ app/src/main/java/com/example/androidfirst/firstapp/FirstAppActivity.kt
@@ -0,0 +1,32 @@
+package com.example.androidfirst.firstapp
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.androidfirst.R
+
+// Definición de tu clase FirstAppActivity que hereda de AppCompatActivity
+class FirstAppActivity : AppCompatActivity() { //FirstAppActivity hereda de AppCompatActivity
+    // Sobrescribiendo el método onCreate que se llama al crear la Activity
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState) // Llamada al método original de la clase padre
+        //enableEdgeToEdge() // Habilita el diseño de borde a borde para esta Activity
+        setContentView(R.layout.activity_first_app) // Establece el diseño de la Activity usando el archivo XML correspondiente
+
+        // Inicia la pantalla desde acá
+
+        /*
+        // Configura un listener que se llama al aplicar los insets de la ventana
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            // Obtiene los insets del sistema (como la barra de estado y la barra de navegación)
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            // Aplica un padding a la vista para que el contenido no sea cubierto por las barras del sistema
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets // Retorna los insets para seguir con el procesamiento de otros listeners
+        }
+
+         */
+    }
+}
