832d5ab89a485f45775d0be46d7b707dc504c74a
martin olmos <martino@epidataconsulting.com>
2024-05-20 16:50:52 -0300

    se agrega nueva pantalla de calculadora IMC

--- app/src/main/java/com/example/androidfirst/MenuActivity.kt
+++ app/src/main/java/com/example/androidfirst/MenuActivity.kt
@@ -8,6 +8,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.androidfirst.calculadoraIMC.CalculadoraIMCActivity
 import com.example.androidfirst.constraintLayoutPractica.ConstraintLayoutPracticaActivity
 import com.example.androidfirst.firstapp.FirstAppActivity
 import com.example.androidfirst.firstapp.ResultActivity
@@ -19,9 +20,11 @@ class MenuActivity : AppCompatActivity() {
 
         val btnPulsar = findViewById<Button>(R.id.btnPrimero)
         val btnPulsar2 = findViewById<Button>(R.id.btnSegundo)
+        val btnPulsar3 = findViewById<Button>(R.id.btnIMCAPP)
 
         btnPulsar.setOnClickListener { navegarPrimerBoton() }
         btnPulsar2.setOnClickListener { navegarSegundoBoton() }
+        btnPulsar3.setOnClickListener { navegarIMCApp() }
 
     }
 
@@ -31,6 +34,11 @@ class MenuActivity : AppCompatActivity() {
     }
     private fun navegarSegundoBoton () {
         val intent = Intent(this, ConstraintLayoutPracticaActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun navegarIMCApp () {
+        val intent = Intent(this, CalculadoraIMCActivity::class.java)
         startActivity(intent)
     }
 }
---
+++ app/src/main/java/com/example/androidfirst/calculadoraIMC/CalculadoraIMCActivity.kt
@@ -0,0 +1,15 @@
+package com.example.androidfirst.calculadoraIMC
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.androidfirst.R
+
+class CalculadoraIMCActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_calculadora_imcactivity)
+    }
+}
