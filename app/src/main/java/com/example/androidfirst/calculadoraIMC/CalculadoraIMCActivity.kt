6b0339057ba8d42b1e6029b3777e9457946e9a87
martin olmos <martino@epidataconsulting.com>
2024-05-23 21:04:36 -0300

    botones de edad y peso

--- app/src/main/java/com/example/androidfirst/calculadoraIMC/CalculadoraIMCActivity.kt
+++ app/src/main/java/com/example/androidfirst/calculadoraIMC/CalculadoraIMCActivity.kt
@@ -11,6 +11,7 @@ import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.androidfirst.R
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.slider.RangeSlider
 
 class CalculadoraIMCActivity : AppCompatActivity() {
@@ -22,6 +23,14 @@ class CalculadoraIMCActivity : AppCompatActivity() {
 
     private lateinit var altura: TextView
     private lateinit var alturaSlider: RangeSlider
+
+    private lateinit var textPeso: TextView
+    private lateinit var btnPesoAdd: FloatingActionButton
+    private lateinit var btnPesoRemove: FloatingActionButton
+
+    private lateinit var textEdad: TextView
+    private lateinit var btnEdadAdd: FloatingActionButton
+    private lateinit var btnEdadRemove: FloatingActionButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,8 +44,17 @@ class CalculadoraIMCActivity : AppCompatActivity() {
     private fun initComponent() {
         this.cardMale = findViewById<CardView>(R.id.cardMale)
         this.cardFemale = findViewById<CardView>(R.id.cardFemale)
+
         this.altura = findViewById<TextView>(R.id.alturaNumero)
         this.alturaSlider = findViewById<RangeSlider>(R.id.rsAltura)
+
+        this.textPeso = findViewById<TextView>(R.id.pesoNumero)
+        this.btnPesoAdd = findViewById<FloatingActionButton>(R.id.btnPesoAdd)
+        this.btnPesoRemove = findViewById<FloatingActionButton>(R.id.btnPesoRemove)
+
+        this.textEdad = findViewById<TextView>(R.id.edadNumero)
+        this.btnEdadAdd = findViewById<FloatingActionButton>(R.id.btnEdadAdd)
+        this.btnEdadRemove = findViewById<FloatingActionButton>(R.id.btnEdadRemove)
     }
 
     private fun initListeners() {
@@ -52,6 +70,30 @@ class CalculadoraIMCActivity : AppCompatActivity() {
             altura.text = value.toInt().toString() + "cm"
         }
 
+        btnPesoAdd.setOnClickListener{
+            //handlePeso(1)
+            textPeso.text = handleEdadYPeso(textPeso.text.toString(), 1)
+        }
+
+        btnPesoRemove.setOnClickListener{
+            //handlePeso(-1)
+            textPeso.text = handleEdadYPeso(textPeso.text.toString(), -1)
+        }
+
+        btnEdadAdd.setOnClickListener{
+            //handleEdad(1)
+            textEdad.text = handleEdadYPeso(textEdad.text.toString(), 1)
+        }
+
+        btnEdadRemove.setOnClickListener{
+            //handleEdad(-1)
+            textEdad.text = handleEdadYPeso(textEdad.text.toString(), -1)
+        }
+
+    }
+
+    private fun handleEdadYPeso (suma:String, numero: Int): String {
+        return if (suma>"0") (suma.toInt()  + numero).toString() else suma
     }
 
     private fun getColor(selected: Boolean): Int {
@@ -81,10 +123,19 @@ class CalculadoraIMCActivity : AppCompatActivity() {
         cardFemale.setCardBackgroundColor(getColor(!this.isMaleSelected))
     }
 
+    private fun inicializarEdad() {
+        textEdad.setText("30")
+    }
+    private fun inicializarPeso() {
+        textPeso.setText("60")
+    }
+
     //este metodo va a setear el color cuando inicie la pantalla,
     // aunque ya este seteado en el layout por defecto en hombre
     private fun renderUI() {
         setCardColor()
+        inicializarEdad()
+        inicializarPeso()
     }
 
 }
