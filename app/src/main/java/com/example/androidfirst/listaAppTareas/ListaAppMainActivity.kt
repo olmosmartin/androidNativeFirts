439416eb45187d9c8d06fa1082a194cc09dea18c
martin olmos <martino@epidataconsulting.com>
2024-05-30 21:09:55 -0300

    funcionalidad agregar tarea con un Dialog

--- app/src/main/java/com/example/androidfirst/listaAppTareas/ListaAppMainActivity.kt
+++ app/src/main/java/com/example/androidfirst/listaAppTareas/ListaAppMainActivity.kt
@@ -1,6 +1,12 @@
 package com.example.androidfirst.listaAppTareas
 
+import android.app.Dialog
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.RadioButton
+import android.widget.RadioGroup
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -11,11 +17,11 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.LayoutManager
 import com.example.androidfirst.R
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class ListaAppMainActivity : AppCompatActivity() {
 
     //le paso las 3 opciones q hice en mi clase sealed TaskCategory para no hardcodearlas en la lista
-    //seria como hacer listOf("business", "personal", "other")
     private var Categorias = listOf(
         TaskCategory.Business,
         TaskCategory.Personal,
@@ -35,17 +41,58 @@ class ListaAppMainActivity : AppCompatActivity() {
     private lateinit var rvTareas: RecyclerView
     private lateinit var tareasAdapter: TareasAdapter
 
+    private lateinit var btnAddTarea: FloatingActionButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lista_app_main)
 
         initComponent()
+        initListeners()
         renderUI()
     }
 
     private fun initComponent() {
         this.rvCategory = findViewById<RecyclerView>(R.id.rvCategorias)
         this.rvTareas = findViewById<RecyclerView>(R.id.rvTareas)
+        this.btnAddTarea = findViewById<FloatingActionButton>(R.id.btnAddTarea)
+    }
+
+    private fun initListeners() {
+        btnAddTarea.setOnClickListener{
+            showDialog()
+        }
+    }
+
+    private fun showDialog() {
+        val dialog = Dialog(this)
+        dialog.setContentView(R.layout.dialog_tareas)
+
+        val btnDialog: Button = dialog.findViewById(R.id.btnDialog)
+        val etAddTarea: EditText = dialog.findViewById(R.id.etAddTarea)
+        val rgAddTarea: RadioGroup = dialog.findViewById(R.id.rgAddTarea)
+
+        btnDialog.setOnClickListener{
+            if (etAddTarea.text.toString().isNotEmpty()) {
+                //obtengo el ID del radiobutton seleccionado
+                val selectedId = rgAddTarea.checkedRadioButtonId
+                //obtengo el radiobutton seleccionado a partir de su ID
+                val selectedRadioButton: RadioButton = rgAddTarea.findViewById(selectedId)
+                //asigna un TaskCategory según el texto q tenga el radio button
+                val CategoriaSeleccionada: TaskCategory = when (selectedRadioButton.text) {
+                    getString(R.string.business) -> TaskCategory.Business
+                    getString(R.string.personal) -> TaskCategory.Business
+                    else -> TaskCategory.Other
+                }
+
+                tareas.add(Tarea(etAddTarea.text.toString(), CategoriaSeleccionada, false))
+                //hay q agregarlo al adapter para q lo pinte en la vida
+                actualizarTarea()
+                dialog.hide()
+            }
+        }
+
+        dialog.show()
     }
 
     private fun renderUI() {
@@ -58,5 +105,9 @@ class ListaAppMainActivity : AppCompatActivity() {
 
         tareasAdapter = TareasAdapter(tareas)
         rvTareas.adapter = tareasAdapter
+    }
+
+    private fun actualizarTarea() {
+        tareasAdapter.notifyDataSetChanged()
     }
 }
